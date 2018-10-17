8de414f767a7672397c011bbc83073d08a82017f
Muhammad Zubair Islam <44185670+zubair1811@users.noreply.github.com>
2018-10-17 18:14:27 +0900

    adding_somone

--- Hello_word.py
+++ Hello_word.py
@@ -1,5 +1,7 @@
 Wellcome to m.zuair islam programming tricks 
 
-
+print ('Hello World!!!!')
+print ('Hi there') 
+##new thing dear###
 x= {print("Hello,world!") for x in range(10)}
-print(x)
+print(x + "Hi there")
